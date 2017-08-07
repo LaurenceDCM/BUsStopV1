6fd9ce56ab569a1170d1614f738fd832f19f5dd7
Laurence Dela Cruz Maiquez <laurence_maiquez@hotmail.com>
2017-08-07 11:17:47 +0800

    Temp working

---
+++ BusStopV1/app/home/home.controller.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    angular.module('app.home')
+        .controller('home_controller', home_controller);
+
+    home_controller.$inject = ['home_factory'];
+
+    function home_controller(home_factory) {
+        var vm = this;
+        vm.stops = [];
+
+        activate();
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 51.509865, lng: -0.118092 },
+            zoom: 5,
+            disableDefaultUI: true
+        });
+
+        var marker2 = new google.maps.Marker({
+            position: { lat: 51.509865, lng: -0.118092 },
+            map: map
+        });
+
+        function activate() {
+            home_factory.getStops(-3.179090, 50.768036, 0.370759, 52.570385).then(function(resp) {
+                vm.stops = resp.data.stops;
+                //atcocode = "340003030CNR"
+
+                angular.forEach(vm.stops, function (val) {
+                    var stopLatLong = { lat: val.latitude, lng: val.longitude };
+
+                    var marker = new google.maps.Marker({
+                        position: stopLatLong,
+                        map: map,
+                        title: val.stop_name
+                    });
+
+                    marker.addListener('click', function () {
+                        map.setZoom(15);
+                        map.setCenter(marker.getPosition());
+                    });
+                });               
+            });
+        }
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 51.509865, lng: -0.118092 },
+            zoom: 12
+        });
+    }
+})();
---
+++ BusStopV1/app/home/home.factory.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    angular.module('app.home')
+        .factory('home_factory', home_factory);
+
+    home_factory.$inject = ['$http'];
+
+    function home_factory($http) {
+        var service = {
+            getStops: getStops,
+            getDeparture: getDeparture
+        };
+
+        var api_key = 'a3f39b8e8c07f321f5e17ccf61968330',
+            app_id = '8aa62f8c';
+
+        return service;
+
+        function getStops(minlon, minlat, maxlon, maxlat) {
+            return $http({
+                method: "GET",
+                contentType: 'application/json',
+                url: 'http://transportapi.com/v3/uk/bus/stops/bbox.json',
+                responseType: 'json',
+                params: {
+                    minlon: minlon,
+                    minlat: minlat,
+                    maxlon: maxlon,
+                    maxlat: maxlat,
+                    api_key: api_key,
+                    app_id: app_id,
+                    page: 2
+                }
+            });
+        }
+
+        function getDeparture(atcocode) {
+            return $http({
+                method: "GET",
+                contentType: 'application/json',
+                url: 'http://transportapi.com/v3/uk/bus/stop/' + atcocode + '/live.json',
+                responseType: 'json',
+                params: {
+                    group: route,
+                    api_key: api_key,
+                    app_id: app_id
+                }
+            });
+        }
+    }
+})();
